5877d7871bf2f83dbcaeb26ef911a02d1755177f
Brandon Levy <brandon.levy@mail.mcgill.ca>
2020-01-13 14:25:56 -0500

    added bounds for the color variables

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -2,23 +2,33 @@ import React, {useState} from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import ColorCounter from '../components/ColorCounter';
 
+const COLOR_INCREMENT = 15;
+
 const SquareScreen = () => {
   const [red, setRed] = useState(0);
   const [green, setGreen] = useState(0);
   const [blue, setBlue] = useState(0);
-  const COLOR_INCREMENT = 15
+
+  const setColorBounds = (colorAmount) => {
+    if (colorAmount >= 255) return 255
+    else if (colorAmount <= 0) return 0
+    else return colorAmount
+  }
+
   return(
     <View>
     <ColorCounter color="Red"
-    onIncrease={()=> {setRed(red + COLOR_INCREMENT)}}
-    onDecrease={()=> {setRed(red - COLOR_INCREMENT)}}
+    onIncrease={()=> {setRed(setColorBounds(red + COLOR_INCREMENT))}}
+    onDecrease={()=> {setRed(setColorBounds(red - COLOR_INCREMENT))}}
     />
     <ColorCounter color="Green"
-    onIncrease={()=> {setGreen(green + COLOR_INCREMENT)}}
-    onDecrease={()=> {setGreen(green - COLOR_INCREMENT)}}/>
+    onIncrease={()=> {setGreen(setColorBounds(green + COLOR_INCREMENT))}}
+    onDecrease={()=> {setGreen(setColorBounds(green - COLOR_INCREMENT))}}
+    />
     <ColorCounter color="Blue"
-    onIncrease={()=> {setBlue(blue + COLOR_INCREMENT)}}
-    onDecrease={()=> {setBlue(blue - COLOR_INCREMENT)}}/>
+    onIncrease={()=> {setBlue(setColorBounds(blue + COLOR_INCREMENT))}}
+    onDecrease={()=> {setBlue(setColorBounds(blue - COLOR_INCREMENT))}}
+    />
     <View style={{ height: 150, width:150, backgroundColor:`rgb(${red}, ${green}, ${blue})` }} />
     </View>
   )
